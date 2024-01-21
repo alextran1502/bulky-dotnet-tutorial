c35b67f7714e94e62aae6da22be7adf7a25dc976
Alex <alex.tran1502@gmail.com>
2024-01-21 16:13:09 -0600

    validation

--- UdemyMVCTutorial/Controllers/CategoryController.cs
+++ UdemyMVCTutorial/Controllers/CategoryController.cs
@@ -7,6 +7,7 @@ namespace UdemyMVCTutorial.Controllers;
 public class CategoryController : Controller
 {
     private readonly ApplicationDbContext _db;
+
     public CategoryController(ApplicationDbContext db)
     {
         _db = db;
@@ -14,7 +15,25 @@ public class CategoryController : Controller
 
     public IActionResult Index()
     {
-        List<Category> objCategoryList = _db.Categories.ToList();
+        var objCategoryList = _db.Categories.ToList();
         return View(objCategoryList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Category obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return View();
     }
 }
--- UdemyMVCTutorial/Models/Category.cs
+++ UdemyMVCTutorial/Models/Category.cs
@@ -1,15 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace UdemyMVCTutorial.Models;
 
 public class Category
 {
-    [Key]
-    public int Id { get; set; }
-    
+    [Key] public int Id { get; set; }
+
+    [DisplayName("Category Name")]
+    [MaxLength(50)]
     [Required]
     public string Name { get; set; }
-    
-    
+
+    [Range(1, 100)]
+    [DisplayName("Display Order")]
     public int DisplayOrder { get; set; }
 }
